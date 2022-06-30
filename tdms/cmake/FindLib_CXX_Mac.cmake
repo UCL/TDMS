19d3e335a007695162a075f3caf8dbc3f81d5e3e
tom <tom.young.316@gmail.com>
2022-06-30 13:54:17 +0100

    Allow ownio

--- tdms/cmake/FindLib_CXX_Mac.cmake
+++ tdms/cmake/FindLib_CXX_Mac.cmake
@@ -1,7 +1,8 @@
 # Find libc++ on a Mac where a specific directory is searched
 # required on an ARM mac targeting x86 and using a non-default compiler
 
-if (${CXX_ROOT})
+if (CXX_ROOT)
+
     find_library(LIBCXX_LIBRARY
             NAMES c++ cxx
             PATHS ${CXX_ROOT}
