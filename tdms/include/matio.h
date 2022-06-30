e471ed75243117dba73c0d147d86c5d07ebd5593
tom <tom.young.316@gmail.com>
2022-06-30 14:23:45 +0100

    Remove ownio and related options

--- tdms/include/matio.h
+++ tdms/include/matio.h
@@ -1,11 +1,6 @@
-#ifndef MIOFLAG
-#include "../matlabio/Mat_io.h"
-#include "tdms_iterator.h"
-#else
 #include "mat.h"
 #include "mex.h"
 #include "matrix.h"
-#endif
 
 #ifdef CLUSTER
 typedef int mwSize; 
