d3f475db4bdbc708ad56cb8cfe056193998fec4b
tom <tom.young.316@gmail.com>
2022-07-28 11:56:22 +0100

    MATLAB unit tests
    Select specific tests
    
    
    Copy data
    
    
    Run tests directly
    
    
    Move delta definition
    
    
    Run command
    
    
    Select tests
    
    
    Remove input file defn
    
    
    Define things as structs
    
    
    Generate full path
    
    
    Print path
    
    
    remove pcb from path
    
    
    Add quotes
    
    
    Show delta path
    
    
    Remove delta path

---
+++ tdms/tests/matlab/README.md
@@ -0,0 +1,7 @@
+### MATLAB tests
+
+The unit tests in this directory can be run with
+
+```bash
+matlab -nodisplay -nosplash -nodesktop -r "run('run_tests.m');exit;"
+```
---
+++ tdms/tests/matlab/run_tests.m
@@ -0,0 +1,15 @@
+import matlab.unittest.TestRunner;
+import matlab.unittest.Verbosity;
+import matlab.unittest.constraints.ContainsSubstring;
+import matlab.unittest.selectors.HasBaseFolder;
+
+addpath(genpath('.'));
+
+suite = testsuite(pwd, 'IncludeSubfolders', true);
+suite = suite.selectIf(HasBaseFolder(ContainsSubstring('tests')));
+
+runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
+results = runner.run(suite);
+
+nfailed = nnz([results.Failed]);
+assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.']);
