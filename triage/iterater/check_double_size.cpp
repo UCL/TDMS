a35a01473edc2ee74c241d72fff140c31e8ee253
Tom Young <39765193+t-young31@users.noreply.github.com>
2022-08-05 14:58:59 +0100

    Remove the triage dir (#40)
    * Remove the triage directory
    
    * Readd and rename ueseful matlab utility scripts

--- triage/iterater/check_double_size.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h> 
-#include <stdlib.h> 
-
-
-int main(int nargs,char *argv[], char **envp){
-  fprintf(stdout,"sizeof(double)=%d\n",sizeof(double));
-}
--- triage/mesh_experiment/genmesh.m
+++
@@ -1,9 +0,0 @@
-%function [vertices, facets] = genmesh([I0 I1 J0 J1 K0 K1])
-function [vertices, facets] = genmesh(cuboidlims)
-    
-    if length(cuboidlims) ~= 6
-	error('Failed to specify cuboid dimensions correctly');
-    end
-    
-    [vertices, facets] = mesh(cuboidlims(1),cuboidlims(2),cuboidlims(3),cuboidlims(4),cuboidlims(5),cuboidlims(6));
-	
