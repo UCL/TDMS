a8fffe1663b75ed8f0785515b665754196707ca3
tom <tom.young.316@gmail.com>
2022-08-08 08:43:07 +0100

    Add wall for clang

--- tdms/src/interpolate.cpp
+++ tdms/src/interpolate.cpp
@@ -1221,7 +1221,6 @@ void interpolateTimeDomainFieldCentralHBandLimited( double ***Hxy, double ***Hxz
     *Hy += bvec[ind1]*hy;
     *Hz += bvec[ind1]*hz;
   }
-  return 0;
 }
 
 /*Interpolate the electric field to the origin of the Yee cell in the time domain
