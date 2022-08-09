7580dcab7e3f732d732ca6ddda57f0b3296ee3e7
willGraham01 <1willgraham@gmail.com>
2022-08-09 13:06:33 +0100

    Refactor exception checks for checkInterpolationPoints

--- tdms/tests/unit/test_interpolate.cpp
+++ tdms/tests/unit/test_interpolate.cpp
@@ -34,14 +34,27 @@ TEST_CASE("checkInterpolationPoints: exceptions thrown") {
     CHECK_THROWS_AS(checkInterpolationPoints(2, I - 2, 2, J - 0, 2, K - 2, I, J, K), runtime_error);
     CHECK_THROWS_AS(checkInterpolationPoints(2, I - 1, 2, J - 2, -1, K - 2, I, J, K), runtime_error);
     CHECK_THROWS_AS(checkInterpolationPoints(2, I - 1, 2, J - 2, 2, K + 1, I, J, K), runtime_error);
+}
 
-    /* When provided input arguments that do not satisfy any of the above - even cases such as i_l > i_u - should pass without throwing an exception */
-    for(int k_l=2; k_l<K-2; k_l++)
-        for(int k_u=2; k_u<K-2; k_u++)
-            for(int j_l=2; j_l<J-2; j_l++)
-                for(int j_u=2; j_u<J-2; j_u++)
-                    for(int i_l=2; i_l<I-2; i_l++)
-                        for(int i_u=2; i_u<I-2; i_u++)
+TEST_CASE("checkInterpolationPoints: correct scheme identified") {
+
+    // setup some fake field dimensions
+    int I = 6, J = 7, K = 8;
+
+    /* checkInterpolationPoints should throw errors in each of the following cases:
+    - i_l < 2 with text Interpolation error: i_l too small
+    - j_l < 2 with text Interpolation error: j_l too small
+    - k_l < 2 with text Interpolation error: k_l too small
+    - i_u > I-2 with text Interpolation error: i_u too large
+    - j_u > J-2 with text Interpolation error: j_u too large
+    - k_u > K-2 with text Interpolation error: k_u too large
+    When provided input arguments that do not satisfy any of the above - even cases such as i_l > i_u - should pass without throwing an exception */
+    for (int k_l = 2; k_l < K - 2; k_l++)
+        for (int k_u = 2; k_u < K - 2; k_u++)
+            for (int j_l = 2; j_l < J - 2; j_l++)
+                for (int j_u = 2; j_u < J - 2; j_u++)
+                    for (int i_l = 2; i_l < I - 2; i_l++)
+                        for (int i_u = 2; i_u < I - 2; i_u++)
                             CHECK_NOTHROW(checkInterpolationPoints(i_l, i_u, j_l, j_u, k_l, k_u, I, J, K));
 }
 
@@ -82,7 +95,7 @@ TEST_CASE("interp: cubic interpolation is exact") {
 
     // equidistant points
     double x[] = {0.,1.,2.,3.};
-    // test acceptence tolerance. Allow for FLOP inprecision and rounding errors
+    // test acceptence tolerance. Allow for FLOP imprecision and rounding errors
     // error should be \approx 4 max(c_i) x^2 __DBL__EPSILON, so order 40 * __DBL__EPSILON__
     double tol = 4e1 * __DBL_EPSILON__;
 
