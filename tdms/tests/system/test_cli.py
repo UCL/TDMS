3d22a9e21250929766bd1b9b1a5e077a4c0e2c13
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2022-09-27 13:52:42 +0100

    Fix the tests.
    Co-authored-by: Will Graham <willGraham01@users.noreply.github.com>
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- tdms/tests/system/test_cli.py
+++ tdms/tests/system/test_cli.py
@@ -3,7 +3,7 @@ from utils import run_tdms
 
 
 def test_help_message_prints():
-
+    """Test that we get the help message"""
     result = run_tdms("-h")
     assert "help" in result.stdout.lower()
 
@@ -11,7 +11,6 @@ def test_help_message_prints():
 @pytest.mark.parametrize("n_args", (1, 4, 5))
 def test_exits_with_incorrect_num_arguments(n_args):
     """Test with the incorrect number of arguments"""
-
     args = n_args * ["a"]
     result = run_tdms(*args)
     assert result.return_code != 0
