55e3d910408330513f583097c2bcf2a363a11aaf
Sam Cunliffe <samcunliffe@users.noreply.github.com>
2022-10-10 12:22:43 +0100

    interpScheme → InterpolationScheme (#128)
    * interpScheme → InterpolationScheme
    
    Also, shorten the function name since its return argument context makes
    things clear. Spotted some missing #pragma once guards and Doxygen file
    comments.
    
    * Update tdms/include/interpolation_methods.h
    
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>

--- tdms/tests/unit/test_interpolation_determination.cpp
+++ tdms/tests/unit/test_interpolation_determination.cpp
@@ -73,18 +73,18 @@ TEST_CASE("checkInterpolationPoints: check valid inputs")
 }
 
 /**
- * @brief Test whether best_interp_scheme correctly determines the appropriate interpolation scheme to use, given the number of Yee cells either side of cell (i,j,k)
+ * @brief Test whether best_scheme correctly determines the appropriate interpolation scheme to use, given the number of Yee cells either side of cell (i,j,k)
  *
  */
-TEST_CASE("best_interp_scheme: correct interpolation chosen")
+TEST_CASE("best_scheme: correct interpolation chosen")
 {
 
     int N = 10;
     // should throw out_of_range exception if interpolation is impossible (<4 Yee cells in direction)
-    REQUIRE_THROWS_AS(best_interp_scheme(3, 2), out_of_range);
+    REQUIRE_THROWS_AS(best_scheme(3, 2), out_of_range);
     // should throw out_of_range exception if Yee cell of invalid index is requested
-    REQUIRE_THROWS_AS(best_interp_scheme(N, 0), out_of_range);
-    REQUIRE_THROWS_AS(best_interp_scheme(N, N), out_of_range);
+    REQUIRE_THROWS_AS(best_scheme(N, 0), out_of_range);
+    REQUIRE_THROWS_AS(best_scheme(N, N), out_of_range);
 
     /* Suppose we have N >= 8 Yee cells in a dimension. The program should determine:
         - cell_id == 0 : Interpolation impossible (checked previously)
@@ -93,15 +93,15 @@ TEST_CASE("best_interp_scheme: correct interpolation chosen")
         - cell_id == N-3,N-2,N-1 : Use BAND_LIMITED_(4,5,6) scheme respectively
     */
     N = 10;
-    CHECK(best_interp_scheme(N, 1).get_priority() == BAND_LIMITED_0);
-    CHECK(best_interp_scheme(N, 2).get_priority() == BAND_LIMITED_1);
-    CHECK(best_interp_scheme(N, 3).get_priority() == BAND_LIMITED_2);
+    CHECK(best_scheme(N, 1).get_priority() == BAND_LIMITED_0);
+    CHECK(best_scheme(N, 2).get_priority() == BAND_LIMITED_1);
+    CHECK(best_scheme(N, 3).get_priority() == BAND_LIMITED_2);
     for (int i = 4; i <= N - 4; i++) {
-        CHECK(best_interp_scheme(N, i).get_priority() == BAND_LIMITED_3);
+        CHECK(best_scheme(N, i).get_priority() == BAND_LIMITED_3);
     }
-    CHECK(best_interp_scheme(N, N - 3).get_priority() == BAND_LIMITED_4);
-    CHECK(best_interp_scheme(N, N - 2).get_priority() == BAND_LIMITED_5);
-    CHECK(best_interp_scheme(N, N - 1).get_priority() == BAND_LIMITED_6);
+    CHECK(best_scheme(N, N - 3).get_priority() == BAND_LIMITED_4);
+    CHECK(best_scheme(N, N - 2).get_priority() == BAND_LIMITED_5);
+    CHECK(best_scheme(N, N - 1).get_priority() == BAND_LIMITED_6);
 
     /* If 4 <= N < 8 we can still fall back on cubic interpolation
         - cell_id == 0 : Interpolation impossible (checked previously)
@@ -110,8 +110,8 @@ TEST_CASE("best_interp_scheme: correct interpolation chosen")
         - cell_id == N-1 : Use CUBIC_LAST
     */
     N = 7;
-    CHECK(best_interp_scheme(N, 1).get_priority() == CUBIC_INTERP_FIRST);
-    for(int i=2; i<=N-2; i++) {CHECK(best_interp_scheme(N, i).get_priority() == CUBIC_INTERP_MIDDLE);}
-    CHECK(best_interp_scheme(N, N-1).get_priority() == CUBIC_INTERP_LAST);
+    CHECK(best_scheme(N, 1).get_priority() == CUBIC_INTERP_FIRST);
+    for(int i=2; i<=N-2; i++) {CHECK(best_scheme(N, i).get_priority() == CUBIC_INTERP_MIDDLE);}
+    CHECK(best_scheme(N, N-1).get_priority() == CUBIC_INTERP_LAST);
 }
 
