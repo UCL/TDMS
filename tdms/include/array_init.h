29c820f0df08008056d67bcf473d5eb49c79d134
Sam Cunliffe <samcunliffe@users.noreply.github.com>
2022-10-18 10:48:37 +0100

    Unused header file. (#134)
    * Unused header file.
    * Move comment to init_grid_arrays.

--- tdms/include/array_init.h
+++ tdms/include/array_init.h
@@ -4,11 +4,19 @@
  * pointers.
  */
 #pragma once
+
 #include "arrays.h"
 #include "field.h"
 #include "mat_io.h"
 #include <string>
 #include <vector>
 
-
+/**
+ * Initialise the grid tensors/arrays, including the electric and magnetic split fields and the
+ * materials array.
+ * @param ptr Pointer to the matlab struct
+ * @param E_s Electric split field
+ * @param H_s Magnetic split field
+ * @param materials Materials array
+ */
 void init_grid_arrays(const mxArray *ptr, SplitField &E_s, SplitField &H_s, uint8_t*** &materials);
