9fcdc340db41b24d8e24c0a1c053cf104fd468af
willGraham01 <1willgraham@gmail.com>
2022-10-31 11:00:44 +0000

    Add docstrings to CellCoordinate

--- tdms/include/cell_coordinate.h
+++ tdms/include/cell_coordinate.h
@@ -12,11 +12,22 @@ private:
   int cell_i = 0, cell_j = 0, cell_k = 0;
 
 public:
+  /**
+   * @brief Construct a new Cell Coordinate object
+   *
+   * @param i,j,k The cell index to assign
+   */
   CellCoordinate(int i = 0, int j = 0, int k = 0) {
     cell_i = i;
     cell_j = j;
     cell_k = k;
   };
+  /**
+   * @brief Construct a new Cell Coordinate object, passing in a contiguous block of memory
+   *
+   * @param indices The indices of the cell index to assign, in the order i,j,k
+   * @param buffer_start First index to read the indices from
+   */
   CellCoordinate(int *indices, int buffer_start = 0) {
     cell_i = indices[buffer_start];
     cell_j = indices[buffer_start + 1];
