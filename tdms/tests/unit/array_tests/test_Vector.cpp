b69ad0da94ced436288907d6bd55a0e98a4e06fc
willGraham01 <1willgraham@gmail.com>
2022-11-09 14:44:40 +0000

    tests for Vector class

--- tdms/tests/unit/array_tests/test_Vector.cpp
+++ tdms/tests/unit/array_tests/test_Vector.cpp
@@ -1,13 +1,13 @@
 /**
  * @file test_Vector.cpp
  * @author William Graham (ccaegra@ucl.ac.uk)
- * @brief Unit tests for the Vector class and its subclasses
+ * @brief Unit tests for the Vector class and its subclasses (FieldComponentsVector)
  */
 #include <catch2/catch_test_macros.hpp>
 #include <spdlog/spdlog.h>
 
 #include "arrays.h"
-#include "matlabio.h"
+#include "mat_io.h"
 
 TEST_CASE("Vector: allocation and deallocation") {
   SPDLOG_INFO("== Testing Vector allocation/deallocation");
@@ -34,4 +34,72 @@ TEST_CASE("Vector: allocation and deallocation") {
 
   // cleanup our MATLAB array
   mxDestroyArray(array);
+}
+
+TEST_CASE("FieldComponentsVector: (horz) allocation and deallocation") {
+  SPDLOG_INFO("== Testing FieldComponentsVector horz allocation/deallocation");
+
+  // FieldComponentsVector can be initialised with the default constructor, then have initialise() called to assign values from a pre-existing MATLAB array
+  FieldComponentsVector v_horz;
+  bool v_horz_ready = ((!v_horz.has_elements()) && (v_horz.size() == 0));
+  REQUIRE(v_horz_ready);
+
+  // create a MATLAB struct with a "components" field to cast to
+  // the constructor shouldn't care whether or not the components field contains an n*1 or 1*n vector either
+  // struct with horizontal vector
+  const int n_fields_horz = 1;
+  const int n_vector_elements_horz = 8;
+  const int horz_struct_dims[2] = {1, 1};
+  const char *horz_struct_fieldnames[n_fields_horz] = {"components"};
+  mxArray *fcv_struct_pointer_horz =
+          mxCreateStructArray(2, (const mwSize *) horz_struct_dims, n_fields_horz, horz_struct_fieldnames);
+  const int vector_dims_horz[2] = {1, n_vector_elements_horz};
+  mxArray *horz_vector =
+          mxCreateNumericArray(2, (const mwSize *) vector_dims_horz, mxINT16_CLASS, mxREAL);
+  int *place_data_horz = (int *) mxGetPr(horz_vector);
+  for (int i = 0; i < n_vector_elements_horz; i++) { place_data_horz[i] = i; }
+  mxSetField(fcv_struct_pointer_horz, 0, horz_struct_fieldnames[0], horz_vector);
+  // initialise
+  v_horz.initialise(fcv_struct_pointer_horz);
+  // in both cases, the number of elements should be n_vector_elements
+  REQUIRE(v_horz.size() == n_vector_elements_horz);
+  // and we should be able to recover the values we assigned
+  for (int i = 0; i < n_vector_elements_horz; i++) { CHECK(v_horz[i] == i); }
+  // destroy what we created
+  mxDestroyArray(fcv_struct_pointer_horz);
+}
+
+TEST_CASE("FieldComponentsVector: (vert) allocation and deallocation") {
+  SPDLOG_INFO("== Testing FieldComponentsVector vert allocation/deallocation");
+
+  // FieldComponentsVector can be initialised with the default constructor, then have initialise() called to assign values from a pre-existing MATLAB array
+  FieldComponentsVector v_vert;
+  bool v_vert_ready = ((!v_vert.has_elements()) && (v_vert.size() == 0));
+  REQUIRE(v_vert_ready);
+
+  // struct with vertical vector
+  const int n_fields_vert = 1;
+  const int n_vector_elements_vert = 8;
+  const int vert_struct_dims[2] = {1, 1};
+  const char *vert_struct_fieldnames[n_fields_vert] = {"components"};
+  mxArray *fcv_struct_pointer_vert = mxCreateStructArray(2, (const mwSize *) vert_struct_dims,
+                                                         n_fields_vert, vert_struct_fieldnames);
+  const int vector_dims_vert[2] = {n_vector_elements_vert, 1};
+  mxArray *vert_vector =
+          mxCreateNumericArray(2, (const mwSize *) vector_dims_vert, mxINT16_CLASS, mxREAL);
+  int *place_data_vert = (int *) mxGetPr(vert_vector);
+  for (int i = 0; i < n_vector_elements_vert; i++) {
+    place_data_vert[i] = n_vector_elements_vert - (i + 1);
+  }
+  mxSetField(fcv_struct_pointer_vert, 0, vert_struct_fieldnames[0], vert_vector);
+  // initialise
+  v_vert.initialise(fcv_struct_pointer_vert);
+  // in both cases, the number of elements should be n_vector_elements
+  REQUIRE(v_vert.size() == n_vector_elements_vert);
+  // and we should be able to recover the values we assigned
+  for (int i = 0; i < n_vector_elements_vert; i++) {
+    CHECK(v_vert[i] == n_vector_elements_vert - (i + 1));
+  }
+  // destroy what we created
+  mxDestroyArray(fcv_struct_pointer_vert);
 }
