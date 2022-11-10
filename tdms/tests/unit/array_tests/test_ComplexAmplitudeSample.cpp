4bf55e089765b40bcf6312fafcfb369e7557e315
willGraham01 <1willgraham@gmail.com>
2022-11-10 15:55:34 +0000

    Tests for ComplexAmplitudeSample

--- tdms/tests/unit/array_tests/test_ComplexAmplitudeSample.cpp
+++ tdms/tests/unit/array_tests/test_ComplexAmplitudeSample.cpp
@@ -15,9 +15,8 @@ TEST_CASE("ComplexAmplitudeSample: allocation and deallocation") {
   SPDLOG_INFO("== Testing ComplexAmplitudeSample class");
 
   // Constructor should just exit if recieving an empty struct
-  // POTENTIALLY DANGEROUS BEHAVIOUR AS WE RE THEN ABLE TO ACCESS OTHER METHODS!!!
   const char *empty_fields[] = {};
-  const int empty_dims[2] = {1, 1};
+  const int empty_dims[2] = {0, 1};
   mxArray *empty_struct_pointer =
           mxCreateStructArray(2, (const mwSize *) empty_dims, 0, empty_fields);
   CHECK_NOTHROW(ComplexAmplitudeSample(empty_struct_pointer));
@@ -33,11 +32,23 @@ TEST_CASE("ComplexAmplitudeSample: allocation and deallocation") {
   // these are the fieldnames that are expected
   const int n_fields = 2;
   const char *fieldnames[n_fields] = {"vertices", "components"};
-  const int n_field_elements = 5;
   // build our struct
   const int dims[2] = {1, 2};
   mxArray *cas_struct_pointer = mxCreateStructArray(2, (const mwSize *) dims, n_fields, fieldnames);
-  // each entry is another structure array, setup for vertices and components
+  // each entry is a numeric array, setup for vertices and components
   // copy code from Vertices and FieldComponentsVector once those tests have been added
+  const int n_elements = 8;
+  // array for "vertices" field
+  mxArray *vertices_array = mxCreateNumericMatrix(n_elements, 3, mxINT32_CLASS, mxREAL);
+  // array for "components" field
+  mxArray *components_vector = mxCreateNumericMatrix(1, n_elements, mxINT16_CLASS, mxREAL);
+  // add fields to struct that will be passed to CAS constructor function
+  mxSetField(cas_struct_pointer, 0, fieldnames[0], vertices_array);
+  mxSetField(cas_struct_pointer, 0, fieldnames[1], components_vector);
+  // create an instance using this struct
+  REQUIRE_NOTHROW(ComplexAmplitudeSample(cas_struct_pointer));
+  // do whatever checks we need
+
+  // destroy what we created
   mxDestroyArray(cas_struct_pointer);
 }
