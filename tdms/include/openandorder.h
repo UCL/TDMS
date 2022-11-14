a025041728921a0dfe6970629c439eee42f03fb3
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2022-11-14 13:13:59 +0000

    Move test_hdf5.
    Move the test_hdf5 function out of openandorder.
    Remove call to example function. Correct doc comment.

--- tdms/include/openandorder.h
+++ tdms/include/openandorder.h
@@ -50,8 +50,3 @@ void check_files_can_be_accessed(ArgumentNamespace &args);
  * @param pointers Pointers to the matrices.
  */
 void assign_matrix_pointers(MatrixCollection &expected, MatFileMatrixCollection &actual, const mxArray **pointers);
-
-/**
- * @brief Test of HDF5 I/O
- */
-void test_hdf5();
