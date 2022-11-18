496a3b81fc220ee72039f3f7a432dc42d3b9fa43
willGraham01 <1willgraham@gmail.com>
2022-11-18 11:26:24 +0000

    CollectionBase tests reworked

--- tdms/tests/include/array_test_class.h
+++ tdms/tests/include/array_test_class.h
@@ -4,6 +4,12 @@
  */
 #pragma once
 
+#include <catch2/catch_test_macros.hpp>
+#include <spdlog/spdlog.h>
+#include <string>
+
+#include "arrays.h"
+
 /**
  * @brief Abstract container for the tests that classes in arrays.h will have to pass.
  *
@@ -11,22 +17,161 @@
  *
  */
 class AbstractArrayTest {
-private:
+protected:
   int empty_dimensions[2] = {0, 1};            //< For initialising empty arrays
   int I_tot = 4, J_tot = 8, K_tot = 4;         //< For mimicing simulation grid dimensions
+  int n_numeric_elements = 8; //< For standardising the number of elements we initialise MATLAB vectors with
   int dimensions_2d[2] = {1, 1};               //< For defining 2D MATLAB arrays
   int dimensions_3d[3] = {I_tot, J_tot, K_tot};//< For defining 3D MATLAB arrays
-  // we could put some common values, EG empty_dimensions, I_tot, etc here as private values to save redefining everything
 
-  void test_empty_construction(){};//< Tests the behaviour of construction when passed an empty MATLAB array
-  void test_correct_construction(){};//< Tests the behaviour of construction methods when passing the expected inputs
+  // To point to the (top-level) matlab inputs the classes require. Top-level structure ensures mxDestroyArray clears all sub-arrays too
+  mxArray *matlab_input = nullptr;
+  // Flags if we have created an mxArray manually, so we are forced to destroy to free memory when done
+  bool matlab_input_assigned = false;
+
+  /**
+   * @brief Destroys the matlab_input array if it is assigned.
+   *
+   * @returns true If mxDestroyArray was called successfully
+   * @returns false matlab_input was not assigned, so no deletion was necessary
+   */
+  bool destroy_matlab_input() {
+    if (matlab_input_assigned) {
+      mxDestroyArray(matlab_input);
+      matlab_input_assigned = false;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  /**
+   * @brief Creates a MATLAB structure array
+   *
+   * @param n_rows,n_cols Dimensions of the array. This is the number of identical structs in the array, not the size of a particular field
+   * @param n_fields Number of fields
+   * @param fields Names of the fields
+   */
+  void create_struct_array(int n_rows, int n_cols, int n_fields, const char *fields[]) {
+    matlab_input = mxCreateStructMatrix(n_rows, n_cols, n_fields, fields);
+    matlab_input_assigned = true;
+  }
+  /**
+   * @brief Creates a 1-by-1 MATLAB structure array
+   *
+   * @param n_fields Number of fields
+   * @param fields Names of the fields
+   */
+  void create_1by1_struct(int n_fields, const char *fields[]) {
+    create_struct_array(1, 1, n_fields, fields);
+  }
+  /**
+   * @brief Creates a MATLAB numeric array
+   *
+   * @param n_dims Number of dimensions in the array
+   * @param dimensions Array/vector containing the number of elements in each dimension, sequentially
+   * @param data_type The MATLAB data type to populate the array with
+   * @param complex_flag Whether the data is real or complex
+   */
+  void create_numeric_array(int n_dims, int *dimensions, mxClassID data_type = mxDOUBLE_CLASS,
+                            mxComplexity complex_flag = mxREAL) {
+    matlab_input =
+            mxCreateNumericArray(n_dims, (const mwSize *) dimensions, data_type, complex_flag);
+    matlab_input_assigned = true;
+  }
+
+  /* Test functions to be overriden by each class.
+  These methods are designed to be overriden by the subclasses specific to each arrays.h class.
+
+  The default behaviour of each test_ method is to return false - this means that subclasses that skip over certain functionality (EG do not need to test the result of providing an empty MATLAB input) are skipped in when run_all_class_tests() is run, and do not bloat the log.
+  If overriden, a test_ method should return true, to indicate to run_all_class_tests() that this test did attempt to run (and might have failed).
+  */
+
+  /**
+  * @brief Tests the behaviour of construction when passed an empty MATLAB array
+  */
+  virtual bool test_empty_construction() { return false;}
+  /**
+   * @brief Tests the behaviour of construction when passed an array of the incorrect dimensions
+   */
+  virtual bool test_wrong_input_dimensions() { return false;}
+  /**
+   * @brief Tests the behaviour of construction when passed a struct with the wrong number of fields
+   */
+  virtual bool test_incorrect_number_of_fields() { return false;}
+  /**
+   * @brief Tests the behaviour of construction methods when passing the expected inputs
+   */
+  virtual bool test_correct_construction() { return false;}
 
 public:
   AbstractArrayTest() = default;
 
-  // Runs all the class's private test methods sequentially
+  /**
+   * @brief Returns the name of the class, for logging purposes
+   */
+  virtual std::string get_class_name() { return "";}
+
+  /**
+   * @brief Runs all the unit tests associated to this class
+   */
   void run_all_class_tests() {
-    test_empty_construction();
-    test_correct_construction();
+    // String to print to the log
+    std::string logging_string = get_class_name() + ": ";
+    // Skips over tests that do not need to be executed
+    bool test_executed;
+
+    // run all tests
+    SECTION("Empty construction") {
+      logging_string += "Empty construction";
+      test_executed = test_empty_construction();
+    }
+    SECTION("Incorrect dimension of input") {
+      logging_string += "Incorrect dimension of input";
+      test_executed = test_wrong_input_dimensions();
+    }
+    SECTION("Incorrect number of fields") {
+      logging_string += "Incorrect number of fields";
+      test_executed = test_incorrect_number_of_fields();
+    }
+    SECTION("Correct construction") {
+      logging_string += "Correct construction";
+      test_executed = test_correct_construction();
+    }
+
+    // suppress output if no test was run
+    if (test_executed) {
+      // tear down is necessary if a test was run
+      bool needed_to_destroy = destroy_matlab_input();
+      if (needed_to_destroy) {
+        logging_string += " (matlab_input destroyed)";
+      } else {
+        logging_string += " (nothing to tear down)";
+      }
+      SPDLOG_INFO(logging_string);
+    }
   }
+};
+
+class CCollectionTest : public AbstractArrayTest {
+  private:
+    const char *fieldnames[9] = { "Cax", "Cay", "Caz", "Cbx", "Cby", "Cbz", "Ccx", "Ccy", "Ccz" };
+
+    bool test_incorrect_number_of_fields() override;
+    bool test_correct_construction() override;
+
+  public:
+    std::string get_class_name() override {return "CCollection";}
+};
+
+class DCollectionTest : public AbstractArrayTest {
+  private:
+    const char *fieldnames[6] = {"Dax", "Day", "Daz", "Dbx", "Dby", "Dbz"};
+
+    std::string class_being_tested = "DCollection";
+
+    bool test_incorrect_number_of_fields() override;
+    bool test_correct_construction() override;
+
+  public:
+    std::string get_class_name() override { return "DCollection"; }
 };
--- tdms/tests/unit/array_tests/test_CollectionBase.cpp
+++ tdms/tests/unit/array_tests/test_CollectionBase.cpp
@@ -7,73 +7,61 @@
 #include <spdlog/spdlog.h>
 
 #include "arrays.h"
+#include "array_test_class.h"
 
-TEST_CASE("CCollection") {
+bool CCollectionTest::test_incorrect_number_of_fields() {
+  create_1by1_struct(3, fieldnames);
+  REQUIRE_THROWS_AS(CCollection(matlab_input), std::runtime_error);
+  return true;
+}
 
-  // setup - an empty mxArray and the names of the field components
-  mxArray *matlab_input;
-  const int n_numeric_elements = 8;
-  const char *fieldnames[] = {"Cax", "Cay", "Caz", "Cbx", "Cby", "Cbz", "Ccx", "Ccy", "Ccz"};
-
-  // we need to provide a struct array with either 6 or 9 fields
-  // incorrect number of fields should throw an error
-  SECTION("Incorrect number of fields") {
-    matlab_input = mxCreateStructMatrix(1, 1, 3, fieldnames);
-    REQUIRE_THROWS_AS(CCollection(matlab_input), std::runtime_error);
-  }
-
-  // now create an array with the correct fieldnames
-  SECTION("Expected input (6 fields)") {
-    matlab_input = mxCreateStructMatrix(1, 1, 6, fieldnames);
-    mxArray *elements6[6];
+bool CCollectionTest::test_correct_construction() {
+  SECTION("(6 inputs)") {
+    create_1by1_struct(6, fieldnames);
+    mxArray *field_arrays[6];
     for (int i = 0; i < 6; i++) {
-      elements6[i] = mxCreateNumericMatrix(1, n_numeric_elements, mxDOUBLE_CLASS, mxREAL);
-      mxSetField(matlab_input, 0, fieldnames[i], elements6[i]);
+      field_arrays[i] = mxCreateNumericMatrix(1, n_numeric_elements, mxDOUBLE_CLASS, mxREAL);
+      mxSetField(matlab_input, 0, fieldnames[i], field_arrays[i]);
     }
     CCollection cc6(matlab_input);
     REQUIRE(!cc6.is_disp_ml);
     REQUIRE(!cc6.is_multilayer);
   }
-  SECTION("Expected input (9 fields)") {
-    matlab_input = mxCreateStructMatrix(1, 1, 9, fieldnames);
-    mxArray *elements9[9];
+  SECTION("(9 inputs)") {
+    create_1by1_struct(9, fieldnames);
+    mxArray *field_arrays[9];
     for (int i = 0; i < 9; i++) {
-      elements9[i] = mxCreateNumericMatrix(2, n_numeric_elements, mxDOUBLE_CLASS, mxREAL);
-      mxSetField(matlab_input, 0, fieldnames[i], elements9[i]);
+      field_arrays[i] = mxCreateNumericMatrix(2, n_numeric_elements, mxDOUBLE_CLASS, mxREAL);
+      mxSetField(matlab_input, 0, fieldnames[i], field_arrays[i]);
     }
     CCollection cc9(matlab_input);
     REQUIRE(cc9.is_disp_ml);
     REQUIRE(cc9.is_multilayer);
   }
+  return true;
+}
 
-  // tear down
-  mxDestroyArray(matlab_input);
+bool DCollectionTest::test_incorrect_number_of_fields() {
+  create_1by1_struct(3, fieldnames);
+  REQUIRE_THROWS_AS(DCollection(matlab_input), std::runtime_error);
+  return true;
+}
+
+bool DCollectionTest::test_correct_construction() {
+  create_1by1_struct(6, fieldnames);
+  mxArray *elements6[6];
+  for (int i = 0; i < 6; i++) {
+    elements6[i] = mxCreateNumericMatrix(1, n_numeric_elements, mxDOUBLE_CLASS, mxREAL);
+    mxSetField(matlab_input, 0, fieldnames[i], elements6[i]);
+  }
+  REQUIRE_NOTHROW(DCollection(matlab_input));
+  return true;
+}
+
+TEST_CASE("CCollection") {
+  CCollectionTest().run_all_class_tests();
 }
 
 TEST_CASE("DCollection") {
-
-  mxArray *matlab_input;
-  const char *fieldnames[] = {"Dax", "Day", "Daz", "Dbx", "Dby", "Dbz"};
-  const int n_numeric_elements = 8;
-
-  // we need to provide a struct array with either 6 or 9 fields
-  // incorrect number of fields should throw an error
-  SECTION("Incorrect number of fields") {
-    matlab_input = mxCreateStructMatrix(1, 1, 3, fieldnames);
-    REQUIRE_THROWS_AS(CCollection(matlab_input), std::runtime_error);
-  }
-
-  // now create an array with the correct fieldnames
-  SECTION("Expected input") {
-    matlab_input = mxCreateStructMatrix(1, 1, 6, fieldnames);
-    mxArray *elements6[6];
-    for (int i = 0; i < 6; i++) {
-      elements6[i] = mxCreateNumericMatrix(1, n_numeric_elements, mxDOUBLE_CLASS, mxREAL);
-      mxSetField(matlab_input, 0, fieldnames[i], elements6[i]);
-    }
-    REQUIRE_NOTHROW(DCollection(matlab_input));
-  }
-
-  // tear down - cleanup MATLAB memory
-  mxDestroyArray(matlab_input);
+  DCollectionTest().run_all_class_tests();
 };
