76e189f9068021e0fb819d222f9e41c8c95e01c0
willGraham01 <1willgraham@gmail.com>
2023-01-25 10:48:46 +0000

    Some missing docstringsg

--- tdms/include/cell_coordinate.h
+++ tdms/include/cell_coordinate.h
@@ -11,10 +11,13 @@ struct ijk {
   // The values (by dimension) that the object contains
   int i = 0, j = 0, k = 0;
 
+  /** @brief Return the maximum of i,j,k */
   int max() { return std::max(std::max(i, j), k); }
+
+  /** @brief Print the (i,j,k) values */
   void print() { spdlog::info("ijk: ({},{},{})", i, j, k); }
 };
 
-// synonyms for code readability
-typedef ijk CellCoordinate;
-typedef ijk IJKDims;
+/* Synonyms for code readability */
+typedef ijk CellCoordinate;//< Index-coordinates (i,j,k) of a Yee cell
+typedef ijk IJKDims;       //< Holds array dimensions (I_tot, J_tot, K_tot)
