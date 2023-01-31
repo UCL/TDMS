68d203e172215c0dda599229fd74f663116eb491
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-01-31 17:09:31 +0000

    Fix TODO about namespace indentation.

--- tdms/include/globals.h
+++ tdms/include/globals.h
@@ -59,8 +59,8 @@ enum PreferredInterpolationMethods { BandLimited, Cubic };
 // **************************************
 
 namespace tdms_math_constants {
-  const double DCPI = 3.14159265358979323846;                                // Pi
-  const std::complex<double> IMAGINARY_UNIT = std::complex<double>(0.0, 1.0);// Imaginary unit
+const double DCPI = 3.14159265358979323846;                                // Pi
+const std::complex<double> IMAGINARY_UNIT = std::complex<double>(0.0, 1.0);// Imaginary unit
 }// namespace tdms_math_constants
 
 // **************************************
@@ -68,8 +68,8 @@ namespace tdms_math_constants {
 // **************************************
 
 namespace tdms_phys_constants {
-  const double EPSILON0 = 8.85400e-12;                        // free space electric permitivity
-  const double MU0 = 4.0 * tdms_math_constants::DCPI * 1.0e-7;// free space magnetic permeability
-  const double LIGHT_V = 1.0 / sqrt(EPSILON0 * MU0);          // free space light velocity
-  const double Z0 = 376.734;                                  // free space inpedance
+const double EPSILON0 = 8.85400e-12;                        // free space electric permitivity
+const double MU0 = 4.0 * tdms_math_constants::DCPI * 1.0e-7;// free space magnetic permeability
+const double LIGHT_V = 1.0 / sqrt(EPSILON0 * MU0);          // free space light velocity
+const double Z0 = 376.734;                                  // free space inpedance
 }// namespace tdms_phys_constants
