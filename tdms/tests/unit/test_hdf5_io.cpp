7c310aa466289b5f9db2c97bac962af179fe8eb1
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-02-07 07:24:19 +0000

    WIP #181. Change the interface a bit. Tests still fail but some because of stupid, not TDD.

--- tdms/tests/unit/test_hdf5_io.cpp
+++ tdms/tests/unit/test_hdf5_io.cpp
@@ -17,98 +17,74 @@
 // tdms
 #include "unit_test_utils.h"
 
-using tdms_tests::create_tmp_dir; // unit_test_utils.h
+using tdms_tests::create_tmp_dir;// unit_test_utils.h
 
 TEST_CASE("Test file I/O construction/destruction.") {
   // setup - temporary directory
   auto tmp = create_tmp_dir();
 
-  SECTION("Check file I/O modes") {
-    // check possible file i/o modes: create file, then open same file readonly, then open
-    // rw, then create it again (even though it already exists)
-    for (auto i : {HDF5FileMode::OVERWRITE, HDF5FileMode::READONLY, HDF5FileMode::READWRITE,
-                   HDF5FileMode::OVERWRITE}) {
-      SPDLOG_DEBUG("Constructing in mode {}", i);
-      HDF5File fi(tmp.string() + "/test_file_constructor.h5", static_cast<HDF5FileMode>(i));
-      CHECK(fi.isOK());
-    }// destructor called as we leave scope
-  }
+  SECTION("Check file creation.") {
+    HDF5Writer f(tmp.string() + "/test_file_constructor.h5");
+    CHECK(f.is_ok());
+  }// destructor called as we leave scope
 
-  SECTION("Check all reasonable file extensions are OK") {
+  SECTION("Check all reasonable file extensions are OK.") {
     for (auto extension : {".hdf5", ".h5", ".mat"}) {
-      HDF5File fi(tmp.string() + "/test_file" + extension, OVERWRITE);
-      CHECK(fi.isOK());
+      {
+        HDF5Writer fw(tmp.string() + "/test_file" + extension);
+        CHECK(fw.is_ok());
+      }// destructor called as we leave scope
+
+      HDF5Reader fr(tmp.string() + "/test_file" + extension);
+      CHECK(fr.is_ok());
     }
   }
 
-  SECTION("Check can't open nonexistent file") {
-    CHECK_THROWS(HDF5File(tmp.string() + "/this_file_doesnt_exist.h5", HDF5FileMode::READONLY));
-  }
-
-  // We should get an exception if we try to write to a readonly file.
-  SECTION("Check can't open file readonly and write to it") {
-    // create a file
-    {
-      HDF5File f1(tmp.string() + "/test_file_read_only.h5", HDF5FileMode::OVERWRITE);
-    }// destructor called as we leave scope
-
-    HDF5File f2(tmp.string() + "/test_file_read_only.h5", HDF5FileMode::READONLY);
-    CHECK_THROWS(f2.write());
+  SECTION("Check can't open nonexistent file.") {
+    CHECK_THROWS(HDF5Reader(tmp.string() + "/this_file_doesnt_exist.h5"));
   }
 
   // Normal operation: we should be able to create a file and write to it, then read from it.
   SECTION("Check write then read.") {
     // create a file
     {
-      HDF5File f1(tmp.string() + "/test_file_wr.h5", HDF5FileMode::OVERWRITE);
-      f1.write();
-      CHECK(f1.isOK());
+      HDF5Writer fw(tmp.string() + "/test_file_wr.h5");
+      double writeme = 1337.;
+      fw.write("testdata", &writeme, 1, (hsize_t*)1);
+      CHECK(fw.is_ok());
 
     }// destructor called as we leave scope
 
-    HDF5File f2(tmp.string() + "/test_file_wr.h5", HDF5FileMode::READONLY);
-    f2.read();
+    HDF5Reader fr(tmp.string() + "/test_file_wr.h5");
+    //fr.read("testdata");
   }
 
   SECTION("Check write then (overwrite) then read.") {
 
     // Create the file and write some data.
     {
-      HDF5File f1(tmp.string() + "/test_file_wwr.h5", HDF5FileMode::OVERWRITE);
-      f1.write();
-      CHECK(f1.isOK());
+      HDF5Writer f1(tmp.string() + "/test_file_wwr.h5");
+      //f1.write();
+      CHECK(f1.is_ok());
 
     }// destructor called as we leave scope
 
     // Overwrite the file and add some different data.
     {
-      HDF5File f2(tmp.string() + "/test_file_wwr.h5", HDF5FileMode::OVERWRITE);
-      f2.write();
-      CHECK(f2.isOK());
+      HDF5Writer f2(tmp.string() + "/test_file_wwr.h5");
+      //f2.write();
+      CHECK(f2.is_ok());
 
     }// destructor called as we leave scope
 
     // Now open the file in readonly. The first data should not be there (and
     // should throw an exception). The second data should be there.
-    HDF5File f3(tmp.string() + "/test_file_wwr.h5", HDF5FileMode::READONLY);
-    f3.read();
+    HDF5Reader f3(tmp.string() + "/test_file_wwr.h5");
+    //f3.read();
 
-    CHECK_NOTHROW(f3.read());
-    CHECK_THROWS(f3.read());
+    //CHECK_NOTHROW(f3.read());
+    //CHECK_THROWS(f3.read());
   }
-
-  // teardown - remove temporary directory and all files
-  SPDLOG_DEBUG("Removing temporary directory.");
-  std::filesystem::remove_all(tmp);
-}
-
-TEST_CASE("Test call example sandbox function") {
-  // setup - temporary directory
-  auto tmp = create_tmp_dir();
-
-  SPDLOG_INFO("Calling the example");
-  example_hdf5();
-  SPDLOG_INFO("Example has run");
 
   // teardown - remove temporary directory and all files
   SPDLOG_DEBUG("Removing temporary directory.");
