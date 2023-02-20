2fe30fe97b5dd84a22ae8dc20b6c5ec16eaac147
willGraham01 <1willgraham@gmail.com>
2023-02-20 12:03:09 +0000

    Remove duplicated functions that are shared in common functionaltiy folder

--- tdms/tests/system/data_generation/prepare_input_generation.m
+++ tdms/tests/system/data_generation/prepare_input_generation.m
@@ -1,6 +1,7 @@
 %% Setup steps for generating the input file to a TDMS system test
 
-% Relative path to the matlab functions that generate the input data
+% Relative path to the shared matlab functions that generate the input data
+% NOTE: If there are function-name conflicts, MATLAB uses the version of the function whose file is located closest to the current working directory. This means that function files in the data_generation folders for each test serve as overrides where necessary for the methods that are defined in the shared functionality folder.
 addpath('../../../../matlab');
 
 % Create directory to place input files in, if it doesn't exist already
