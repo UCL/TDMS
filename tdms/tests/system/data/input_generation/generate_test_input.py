9c3d45f360979bdcd50a62626490e8c95e9a8c94
willGraham01 <1willgraham@gmail.com>
2023-02-24 16:27:12 +0000

    Update to use matlabengine for cleaner calls

--- tdms/tests/system/data/input_generation/generate_test_input.py
+++ tdms/tests/system/data/input_generation/generate_test_input.py
@@ -1,21 +1,24 @@
 import os
 from glob import glob
 from pathlib import Path
-from subprocess import PIPE, Popen
 from typing import Union
 
+import matlab.engine as matlab
 import yaml
+from matlab.engine import MatlabEngine
 
 LOCATION_OF_THIS_FILE = os.path.dirname(os.path.abspath(__file__))
-# MATLAB executable path
-MATLAB_PATH = "matlab"
 # Additional options for running matlab on the command-line
-MATLAB_OPTS = ["-nodisplay", "-nodesktop", "-nosplash", "-r"]
+MATLAB_OPTS_LIST = ["-nodisplay", "-nodesktop", "-nosplash", "-r"]
+MATLAB_STARTUP_OPTS = " ".join(MATLAB_OPTS_LIST)
 # Paths to matlab functions not in LOCATION_OF_THIS_FILE
 MATLAB_EXTRA_PATHS = [
     os.path.abspath(LOCATION_OF_THIS_FILE + "/bscan"),
     os.path.abspath(LOCATION_OF_THIS_FILE + "/matlab"),
 ]
+
+
+engine: MatlabEngine = matlab.start_matlab(MATLAB_STARTUP_OPTS)
 
 
 class BScanArguments:
@@ -33,101 +36,93 @@ class BScanArguments:
     input_filename: Union[str, Path]
 
     def __init__(self, test_directory, input_filename) -> None:
-        """Initialise by unpacking values."""
+        """Initialise by unpacking values, and assigning defaults if necessary."""
         self.test_directory = test_directory
         self.input_filename = input_filename
         return
 
-    def create_bscan_argument(self) -> str:
-        """Create the syntax of the call to run_bscan.m, but inserting the input-argument values in place.
-        Also inserts "default" values if appropriate, to avoid doing this in matlab with varargin.
+    def run_bscan(self, engine: MatlabEngine) -> None:
+        """Runs the run_bscan function in the MatlabEngine provided.
 
-        The result is a string of the form:
-        run_bscan(input_arguments_in_the_correct_order),
-
-        where the input arguments are replaced with their values that need to be passed into the corresponding matlab call.
+        The bscan/ and matlab/ directories are assumed to already be in the includepath of the engine instance, so that the run_bscan and supporting matlab files can be called.
         """
-        bscan_args = []
-        bscan_args += ["'" + str(self.test_directory) + "'"]
-        bscan_args += ["'" + str(self.input_filename) + "'"]
-
-        bscan_arg_string = "run_bscan(" + ",".join(bscan_args) + ")"
-        return bscan_arg_string
+        # function [] = run_bscan(test_directory, input_filename)
+        # Cast to str() to guard against Path instances slipping through
+        engine.run_bscan(str(self.test_directory), str(self.input_filename), nargout=0)
+        return
 
 
-class MATLABCommand:
-    """A class that allows us to create a matlab command that can be run in a shell.
+class MATLABEngineWrapper:
+    """When we regenerate input data, we always need to add the bscan/ and matlab/ directories to the MATLAB instance's search path. correspondingly, we always want to kill the MATLAB instance after running the run_bscan function and generating the data.
 
-    Serves as a wrapper for the input data generation. An instance of this class stores the commands we would like to execute in matlab_commands (list) to create the input data, in the desired order of execution. Commands for adding the paths to the .m files needed for data generation and to exit matlab after concluding are always the first and last elements of this list, and are automatically (pre/a)-ppended.
-
-    The .run() method creates a shell instance that runs the MATLAB commands specified by the class instance, by converting the matlab_commands list into a string which is then passed to matlab on the command-line. The member variables stdout and return_code provide debugging information for the shell process that runs.
+    This class serves as a wrapper for that purpose. It stores instance(s) of the BScanArguments class, which it will run in sequence between the aforementioned addpath() setup and then engine shutdown. The .run() method performs exactly this.
     """
 
     # List containing the subsequent matlab commands to be executed by the interpreter
-    matlab_commands: list[str]
-    # Stdout from running this command
-    stdout: str
-    # Shell exit code after running this matlab command
-    return_code: int
+    bscan_calls: list[BScanArguments]
 
-    def __init__(self, matlab_args: Union[BScanArguments, list[str]]) -> None:
-        """Initialise by being told the arguments that the run_bscan function will need.
+    # The MATLAB session that will run
+    engine: MatlabEngine
 
-        Then wrap a call to this command between addpath(), so that matlab can find the run_bscan function itself, and exit, so that matlab does not hang after running the bscan function.
-
-        If the input is a BScanArguments object, the constructor can assemble the matlab call to run_bscan by itself.
-        If the input is a list[str], then the constructor interprets this as a list of matlab commands to be run, in the list order.
-        """
-        self.matlab_commands = [self._addpath_commands()]
+    def __init__(self, bscans: Union[BScanArguments, list[BScanArguments]]) -> None:
+        """Initialise with the provided (list of) BScanArguments."""
+        self.bscan_calls = []
         # add the additional commands here
-        if isinstance(matlab_args, BScanArguments):
+        if isinstance(bscans, BScanArguments):
             # handed BScanArgument, construct command from this
-            self.matlab_commands += [matlab_args.create_bscan_argument()]
+            self.bscan_calls = [bscans]
+        elif isinstance(bscans, list):
+            # validate this is a list of BScanArguments and not some other datatype
+            non_bscan_items = sum([isinstance(b, BScanArguments) for b in bscans])
+            if non_bscan_items > 0:
+                raise RuntimeError(
+                    f"Error: not all inputs are BScan calls ({non_bscan_items}/{len(bscans)})"
+                )
+            else:
+                self.bscan_calls = bscans
         else:
-            # handed a list of strings, which are the individual matlab arguments
-            self.matlab_commands += matlab_args
-        # add the exit command because MATLAB does not give up control of the system after running a script >:(
-        self.matlab_commands += [self._exit_command()]
+            # I don't know what we've been handed, but it's not what I was expecting
+            raise RuntimeError(
+                f"Error: expected BScanArguments or list[BScanArguments] but got {type(bscans)}"
+            )
+        # do not start the engine yet
+        self.engine = None
+        return
 
-    @staticmethod
-    def _addpath_commands() -> str:
-        """Produces a string whose text is a call to addpath() in matlab, so that matlab can find the run_bscan.m file and other supporting .m files needed for input generation."""
-        command_str: str = "addpath("
-        for extra_path in MATLAB_EXTRA_PATHS:
-            command_str += "'" + extra_path + "',"
-        # remove the extra comma after the last path was added
-        command_str = command_str[:-1] + ")"
-        return command_str
+    def _addpath_commands(self) -> None:
+        """Adds the bscan/ and matlab/ directories to the session's path."""
+        for path in MATLAB_EXTRA_PATHS:
+            self.engine.addpath(path)
+        return
 
-    @staticmethod
-    def _exit_command() -> str:
-        """Produces a string that corresponds to the exit command, which when run in matlab will close the instance."""
-        return "exit"
+    def _stop_engine(self) -> None:
+        """Kills engine if it is still running."""
+        self.engine.quit()
+        return
 
-    def create_command_string(self) -> str:
-        """Sandwich each separate matlab command in the matlab_commands list into a single string matlab_command_string, which can be passed to matlab on the command line as:
-        matlab -r matlab_command_string
+    def run(self, kill_on_complete: bool = True) -> None:
+        """Run the bscan arguments saved to the instance, in the same session.
 
-        The string returned is matlab_command_string.
+        Engine is terminated if kill_on_complete is True, otherwise the engine is left running and manual cleanup is needed. It can be useful for debugging to leave the engine running, however it is recommended to stop it after we have finished using it.
         """
-        matlab_command_string = "; ".join(self.matlab_commands) + ";"
-        return matlab_command_string
+        # If we have no bscan arguments to run, don't bother starting the engine and report this
+        if len(self.bscan_calls) == 0:
+            raise RuntimeWarning(
+                "No bscan calls specified in this instance. Engine not started."
+            )
+        else:
+            # Start the engine
+            self.engine = matlab.start_matlab(MATLAB_STARTUP_OPTS)
+            # Add necessary paths
+            self._addpath_commands()
 
-    def run(self) -> None:
-        """Run the following matlab scripts/functions, in order, in THE SAME SESSION BECAUSE PATHING THINGS:
+            # Run every bscan call
+            for b in self.bscan_calls:
+                b.run_bscan(self.engine)
 
-        addpath(EXTRA_MATLAB_PATHS)         <- run_bscan function & other required functions will be placed on the path
-        run_bscan(<CORRECT CALL OPTIONS>)   <- populate call options with stuff from the config file
-        exit                                <- prevent matlab from hanging after generating the data
-        """
-        # Generate the command-line call
-        args = MATLAB_OPTS + [self.create_command_string()]
-        # Run the matlab command
-        p = Popen([MATLAB_PATH, *args], stdout=PIPE, cwd=LOCATION_OF_THIS_FILE)
-        matlab_infodump, _ = p.communicate()
-        # Save outputs for debugging later
-        self.return_code = p.returncode
-        self.stdout = matlab_infodump.decode()
+            # If requested, kill the instance
+            if kill_on_complete:
+                self._stop_engine()
         return
 
 
@@ -136,7 +131,7 @@ class GenerationData:
 
     Information is read from a config.yaml file, and the contents of the "generation" field are validated against what is expected.
 
-    From this information, the matlab_command is created. This command is the equivalent of running the old run_{pstd,fdtd}_bscan.m scripts on the test in question, however it is now sufficiently generalised so that we do not need an individual run file for each test.
+    From this information, the call to run_bscan is created. This command is the equivalent of running the old run_{pstd,fdtd}_bscan.m scripts on the test in question, however it is now sufficiently generalised so that we do not need an individual run file for each test.
 
     The input data for the particular test can then be generated using the .generate() method on the class instance.
     """
@@ -158,16 +153,15 @@ class GenerationData:
     # The spatial obsticles that are present in the runs of this test
     obstacles: list[str]
 
-    # The matlab command that generates the input data for this test
-    matlab_command: MATLABCommand
+    # The matlab instance that generates the input data for this test
+    matlab_instance: MATLABEngineWrapper
 
     def __init__(self, config_file_location: Union[str, Path]) -> None:
         """Initialise instance by reading information from a config file"""
         # Fetch config file and options
         self._config_file_location = Path(config_file_location)
-        config_file = open(config_file_location, "r")
-        config = yaml.safe_load(config_file)
-        config_file.close()
+        with open(config_file_location, "r") as config_file:
+            config = yaml.safe_load(config_file)
 
         # Get the ID of the test & thus the directory to save to
         self.test_id = config["test_id"]
@@ -180,7 +174,7 @@ class GenerationData:
         self._setup_member_vars_from_config()
 
         # Setup the command from the member variables
-        self.matlab_command = self._setup_matlab_command()
+        self.matlab_instance = self._setup_matlab_instance()
         return
 
     def _validate_config_options(self) -> None:
@@ -221,12 +215,12 @@ class GenerationData:
         # else, the directory already exists, we don't need to do anything
         return
 
-    def _setup_matlab_command(self) -> MATLABCommand:
+    def _setup_matlab_instance(self) -> MATLABEngineWrapper:
         """Setup the matlab command that will generate the input data"""
-        bscan_args = BScanArguments(self.test_dir, self.input_file)
-        return MATLABCommand(bscan_args)
+        bscan = BScanArguments(self.test_dir, self.input_file)
+        return MATLABEngineWrapper(bscan)
 
-    def generate(self) -> int:
+    def generate(self) -> None:
         """Generate the input data to the test, as specified by this instance's member values.
 
         Returns the exit code of the sequence of matlab commands that were run."""
@@ -234,9 +228,9 @@ class GenerationData:
         self._find_or_create_test_dir()
 
         # generate the input data for this test
-        self.matlab_command.run()
+        self.matlab_instance.run()
 
         # cleanup auxillary .mat files that are placed into this directory
         for aux_mat in sorted(glob(LOCATION_OF_THIS_FILE + "/*.mat")):
             os.remove(aux_mat)
-        return self.matlab_command.return_code
+        return
