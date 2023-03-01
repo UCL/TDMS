110914ecac9b6f3fd9ce36dedb2200f2c16e45b0
willGraham01 <1willgraham@gmail.com>
2023-03-01 15:38:25 +0000

    Revert unnecessary additions

--- tdms/tests/system/data/input_generation/matlab/efield_focused_equiv.m
+++
@@ -1,7 +0,0 @@
-%function [E] = efield(X,Y,Z);
-%
-%This function is called by iteratefdtd_matrix to set the electric
-%field source terms
-%X, Y and Z should bu in microns
-function [E] = efield_focused_equiv(X,Y,Z)
-    E = {zeros(size(X)),zeros(size(X)),zeros(size(X))};
