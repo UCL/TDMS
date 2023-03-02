938d404264bd4a718d02a846651335f6697556ac
willGraham01 <1willgraham@gmail.com>
2023-03-02 12:16:03 +0000

    Tidy multi_layer and yeeposition
    - yeeposition avoids more code duplication in each component case
    - multi_layer now has a proper docstring and formatting, but it is not clear what it actually does still

--- tdms/tests/system/data/input_generation/matlab/yeeposition.m
+++ tdms/tests/system/data/input_generation/matlab/yeeposition.m
@@ -1,34 +1,32 @@
-%function [x y z] = yeeposition(i,j,k,delta,component)
-%
-%Calculate the position in a cartesian grid of the field
-%component specified by the yee cell index (i,j,k). delta
-%is a struct with members:
-%delta.x, delta.y and delta.z which are the yee cell
-%dimension and component is the component of interest
 function [x, y, z] = yeeposition(i,j,k,delta,component)
+	%% Calculate the position in cartesian space of the field component associated to the Yee cell with index (i,j,k).
+	%% E-field components are offset by 0.5 * the Yee cell extent in their dimension.
+	%% H-field components are offset by 0.5 * the Yee cell extent in the OTHER two dimensions.
+	% i, j, k	: Indicies of the Yee cell.
+	% delta		: Struct with members x, y, z, which are the Yee cell extent in the corresponding axial direction.
+	% component	: String, the field component whose position we wish to know.
+	%
+	% x, y, z	: The cartesian coordinates of the field component.
 
-    if strcmp(component,'Ex')
-	x = (i+0.5)*delta.x;
-	y = j*delta.y;
-	z = k*delta.z;
-    elseif strcmp(component,'Ey')
-	x = i*delta.x;
-	y = (j+0.5)*delta.y;
-	z = k*delta.z;
-    elseif strcmp(component,'Ez')
-	x = i*delta.x;
-	y = j*delta.y;
-	z = (k+0.5)*delta.z;
-    elseif strcmp(component,'Hx')
-	x = i*delta.x;
-	y = (j+0.5)*delta.y;
-	z = (k+0.5)*delta.z;
-    elseif strcmp(component,'Hy')
-	x = (i+0.5)*delta.x;
-	y = j*delta.y;
-	z = (k+0.5)*delta.z;
-    elseif strcmp(component,'Hz')
-	x = (i+0.5)*delta.x;
-	y = (j+0.5)*delta.y;
-	z = k*delta.z;
-    end
+%% Field component coordinate alligns with Yee cell centre for all other components
+x = i * delta.x;
+y = j * delta.y;
+z = k * delta.z;
+%% Add offsets based on which component we are examining
+if strcmp(component,'Ex')
+	x = x + 0.5*delta.x;
+elseif strcmp(component,'Ey')
+	y = y + 0.5*delta.y;
+elseif strcmp(component,'Ez')
+	z = z + 0.5*delta.z;
+elseif strcmp(component,'Hx')
+	y = y + 0.5*delta.y;
+	z = z + 0.5*delta.z;
+elseif strcmp(component,'Hy')
+	x = x + 0.5*delta.x;
+	z = z + 0.5*delta.z;
+elseif strcmp(component,'Hz')
+	x = x + 0.5*delta.x;
+	y = y + 0.5*delta.y;
+end
+end
