82ce67a8d8eead8a26702370188845062e5747fe
willGraham01 <1willgraham@gmail.com>
2023-03-02 10:40:51 +0000

    Update and tidy fdtd_bounds
    - No longer reads the input file line by line, and eval's each line in turn. Instead, now checks the input file exists and runs it as a script (which is functionally identical to before, but is much more robust and easier to read as a human)
    - Required variables are checked for in a while loop that breaks when all variables have been found
    - Optional variable z_launch has its existence checked, rather than being a nested exception to the required variables
    - Docstrings explaining what the function actually does have been included

--- tdms/tests/system/data/input_generation/matlab/fdtd_bounds.m
+++ tdms/tests/system/data/input_generation/matlab/fdtd_bounds.m
@@ -1,50 +1,49 @@
-%function [x,y,z,lambda] = fdtd_bounds(input_file)
-%
-%input_file - file with input configuration information
-%
-%x, y and z are the grid labels of the interior space of the FDTD
-%grid.
-%
-%lambda is the wavelength in dielectric material
 function [x,y,z,lambda] = fdtd_bounds(input_file)
+	%% Compute vectors x, y, z defining the coordinates of the fdtd grid in the respective axial direction.
+	%% That is, the Cartesian product x \otimes y \otimes z is the set of all gridpoints (xi, yj, zk) in the fdtd grid.
+	% input_file	: The input file with configuration information.
+	%
+	% x, y, z		: Grid labels (spatial coodinates) of the interior space of the FDTD grid.
+	% lambda		: Wavelength of light in the dielectric material.
 
-%input the configuration information
-[fid_input,message] = fopen(input_file,'r');
+%% Fetch the configuration information for this test
 
-%check if file was opened successfully
-if fid_input== -1
+% Check that the input file can be found on the path
+if isfile(input_file)
+	% Run input file as a script to import variables into the workspace
+	run(input_file);
+else
+	% Throw error - config file cannot be found
     error(sprintf('File %s could not be opened for reading',input_file));
 end
 
-%proceed to_l read in config information
-current_line = fgets(fid_input);
-
-while current_line ~= -1
-    eval(current_line);
-    current_line = fgets(fid_input);
-end
-
-%now need to check that all of the required variables have been set
-variables = {'delta','I','J','K','z_launch','illorigin','lambda'};
-must_abort = 0; %assumes all variables have been defined
-for lvar = 1:length(variables)
-    if exist(variables{lvar}) ~= 1
-	if strncmp(variables(lvar),'z_launch',8)
-	    fprintf(1,'Failed to define %s, setting it to 0\n',variables{lvar});
-	    z_launch = 0;
-	else
-	    fprintf(1,'Failed to define %s\n',variables{lvar});
-	    must_abort = 1;
+% Check required variables have been set in the config file, and loaded
+% Note that lambda will be returned once it is imported from the input file
+required_variables = {'delta','I','J','K','z_launch','illorigin','lambda'};
+n_required_variables = length(required_variables);
+i = 1;
+% Search through all required variables and check they exist in the workspace
+% Throw error (and break loop early) if they are not found
+while i<=n_required_variables
+	if ~exist(required_variables{i}, 'var')
+		error(sprintf('Required variable %s not present in %s', required_variables{i}, input_file));
+		break
 	end
-    end
+	i = i + 1;
 end
 
-if must_abort
-    error('Not all variables were defined');
+% These variables have default values if they are not defined in the input_file
+if ~exist('z_launch', 'var')
+	% z_launch defines an additional "height" above the illumination origin
+	z_launch = 0;
 end
 
-
+%% Now compute the fdtd grid coordinates along each axis. Values are (respective to each axial direction):
+% I, J, K 		: Number of gridpoints
+% delta,{x,y,z} : Spatial separation of the gridpoints
+% illorigin		: Location of the origin of the source field
 x = ((1:I) - illorigin(1))*delta.x;
 y = ((1:J) - illorigin(2))*delta.y;
 z = ((1:K) - illorigin(3))*delta.z + z_launch;
-lambda = lambda;
+
+end
