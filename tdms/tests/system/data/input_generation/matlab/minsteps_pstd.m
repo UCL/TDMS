bf0b05c28b005fe765a7f693214933256bff76f8
willGraham01 <1willgraham@gmail.com>
2023-03-02 11:57:44 +0000

    Tidy minsteps_fdtd and minsteps_pstd
    - These files are basically identical, so could quite easily be combined
    - Regardless, tidies up their docstrings and imports
    - fdtdbounds was missing a required argument

--- tdms/tests/system/data/input_generation/matlab/minsteps_pstd.m
+++ tdms/tests/system/data/input_generation/matlab/minsteps_pstd.m
@@ -1,56 +1,48 @@
-%function [n] = fdtdminsteps(inputfile)
-%
-%estimates the minmum number of steps required in a fdtd simulation
-%assuming the time step used is 0.95xmaximum time step and that the
-%trailing edge of the guassian pulse travels at the speed of
-%light. (dispersion free). Calculates the edge of the pulse travelling from
-%interface to end wall and back.
-function [n] = minsteps_pstd(inputfile)
+function [n] = minsteps_pstd(input_file)
+    %% Estimates the minmum number of steps required in a pstd simulation.
+    %% Assumes the time step used is 0.95 * maximum time step, and that the trailing edge of the guassian pulse travels at the speed of light (dispersion free).
+    %% Calculates the edge of the pulse travelling from the interface to the end wall and back.
+    % input_file    : Config file to read values from
+    %
+    % n             : Minimum number of timesteps that will need to be performed in a fdtd simulation
 
-[fid_input,message] = fopen(inputfile,'r');
+%% Fetch the configuration information for this test
 
-%check if file was opened successfully
-if fid_input== -1
+% Check that the input file can be found on the path
+if isfile(input_file)
+	% Run input file as a script to import variables into the workspace
+	run(input_file);
+else
+	% Throw error - config file cannot be found
     error(sprintf('File %s could not be opened for reading',input_file));
 end
 
-%proceed to_l read in config information
-current_line = fgets(fid_input);
-
-while current_line ~= -1
-    eval(current_line);
-    current_line = fgets(fid_input);
+% Check required variables have been set in the config file, and loaded
+% Note that lambda will be returned once it is imported from the input file
+required_variables = {'delta','K','interface','f_an','dt','epsr'};
+n_required_variables = length(required_variables);
+i = 1;
+% Search through all required variables and check they exist in the workspace
+% Throw error (and break loop early) if they are not found
+while i<=n_required_variables
+	if ~exist(required_variables{i}, 'var')
+		error(sprintf('Required variable %s not present in %s', required_variables{i}, input_file));
+		break
+	end
+	i = i + 1;
 end
 
-[epso muo c] = import_constants;
+%% Load / compute parameters required
+[~, ~, c] = import_constants;
+[t0, hwhm] = fdtdduration(inputfile);
 
-if exist('K') ~= 1
-    error('K is not defined - cannot determine n');
-end
+% Adjust so that we have the pulse close to 0 at the interface
+t = 2*t0;
 
-if exist('delta') ~= 1
-    error('delta is not defined - cannot determine n');
-end
-
-if exist('interface') ~= 1
-    error('interface is not defined - cannot determine n');
-end
-
-if exist('f_an') ~= 1
-    error('f_an is not defined - cannot determine n');
-end
-
-if exist('dt') ~= 1
-    error('dt is not defined - cannot determine n');
-end
-
-[to hwhm] = fdtdduration(inputfile);
-
-%have to have pulse reaching close to 0 at the interface
-t = 2*to;
-
-%now this has to propagate from the interface to the other edge of
-%the grid and then out again
+%% Compute timestep estimate
+% This pulse has to propagate from the interface, to the other edge of the grid, and then out again
+% This is the time that will be ellapsed
 t = t + (K-interface.K1(1))*delta.z/(c/sqrt(epsr(1))) + K*delta.z/(c/sqrt(epsr(1)));
-
+% Hence from the ellapsed time, we can compute the number of timesteps we need for the pulse to travel back & forth
 n = ceil(t/(0.95*dt));
+end
