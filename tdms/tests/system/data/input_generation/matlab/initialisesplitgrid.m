96c2d5e7777a2ae21db97ec6ff23a7aea3330df8
willGraham01 <1willgraham@gmail.com>
2023-03-02 11:36:09 +0000

    Tidy up initialiseslitgrid further by removing explicit typeouts

--- tdms/tests/system/data/input_generation/matlab/initialisesplitgrid.m
+++ tdms/tests/system/data/input_generation/matlab/initialisesplitgrid.m
@@ -6,7 +6,7 @@ function [fdtdgrid] = initialisesplitgrid(I,J,K,Dxl,Dxu,Dyl,Dyu,Dzl,Dzu)
     % I, J, K       : Number of non- PML Yee cells x, y, z direction
     % Dx, Dy, Dz    : Number of cells in x, y, z direction in a single PML layer
     %
-    % fdtdgrid : Struct with 13 elements:
+    % fdtdgrid : 1-by-1 struct with 13 elements:
     % Exy, Exz, Eyx, Eyz, Ezx, Ezy, Hxy, Hxz, Hyx, Hyz, Hzx, Hzy : The split-field components
     % material                                                   : Flags whether the cell is PML or not
     %
@@ -14,21 +14,16 @@ function [fdtdgrid] = initialisesplitgrid(I,J,K,Dxl,Dxu,Dyl,Dyu,Dzl,Dzu)
     % A material type of 0 means that the material is either free space or a pml cell, and the cell index may be used to index into the appropriate vector for the update parameter.
     % If the material type is greater than zero, the update paramter is found by indexing into the material matrix.
 
+%% Compute total number of cells
 I_tot = I + Dxl + Dxu;
 J_tot = J + Dyl + Dyu;
 K_tot = K + Dzl + Dzu;
 
-fdtdgrid.Exy = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Exz = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Eyx = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Eyz = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Ezx = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Ezy = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Hxy = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Hxz = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Hyx = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Hyz = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Hzx = zeros(I_tot+1,J_tot+1,K_tot+1);
-%fdtdgrid.Hzy = zeros(I_tot+1,J_tot+1,K_tot+1);
+%% Create structure array
+split_field_components = {'Exy', 'Exz', 'Eyx', 'Eyz', 'Ezx', 'Ezy', 'Hxy', 'Hxz', 'Hyx', 'Hyz', 'Hzx', 'Hzy'};
+fdtdgrid = struct();
+for i=1:numel(split_field_components)
+    fdtdgrid.(split_field_components{i}) = zeros(I_tot+1,J_tot+1,K_tot+1);
+end
 fdtdgrid.materials = uint8(zeros(I_tot+1,J_tot+1,K_tot+1));
 end
