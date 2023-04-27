09c61e274d5eb5cb1d78b32ac7c3410f52cea72d
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-04-27 12:31:30 +0100

    Go for 100%.

--- tdms/tests/unit/test_argument_parser.cpp
+++ tdms/tests/unit/test_argument_parser.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <stdexcept>
 #include <string>
+#include <unistd.h>
 #include <vector>
 
 #include <catch2/catch_test_macros.hpp>
@@ -15,7 +16,8 @@ using std::string;
 using std::vector;
 
 /**
- * @brief Test that the argument parser recovers the input arguments provided.
+ * @brief Test that the argument namespace recovers the input arguments
+ * provided.
  */
 TEST_CASE("Test parsing help") {
   const char *input_args[] = {"tdms", "-h"};
@@ -45,8 +47,9 @@ TEST_CASE("Test parsing two filenames") {
 
   const vector<string> input_args = {"tdms", "input_file.mat",
                                      "output_file.mat"};
-  auto args = ArgumentNamespace(
-          input_args.size(), const_cast<char **>(vector_to_array(input_args)));
+  ArgumentParser ap;
+  auto args = ap.parse_args(input_args.size(),
+                            const_cast<char **>(vector_to_array(input_args)));
 
   SECTION("Inputs") {
     vector<string> expected_filenames = drop_first_and_last(input_args);
@@ -70,8 +73,9 @@ TEST_CASE("Test parsing two filenames") {
 TEST_CASE("Test parsing three filenames") {
   vector<string> input_args = {"tdms", "input_file.mat", "grid_file.mat",
                                "output_file.mat"};
-  auto args = ArgumentNamespace(
-          input_args.size(), const_cast<char **>(vector_to_array(input_args)));
+  ArgumentParser ap;
+  auto args = ap.parse_args(input_args.size(),
+                            const_cast<char **>(vector_to_array(input_args)));
 
   SECTION("Input") {
     vector<string> expected_filenames = drop_first_and_last(input_args);
@@ -100,16 +104,71 @@ TEST_CASE("Test wrong number of file names") {
     const char *input_args[] = {"tdms"};
     auto args = ArgumentNamespace(1, const_cast<char **>(input_args));
     REQUIRE(!args.have_correct_number_of_filenames());
+    REQUIRE_THROWS_AS(args.input_filename(), std::runtime_error);
+    REQUIRE_THROWS_AS(args.grid_filename(), std::runtime_error);
+    REQUIRE_THROWS_AS(args.output_filename(), std::runtime_error);
   }
   SECTION("One") {
     const char *input_args[] = {"tdms", "input_file.mat"};
     auto args = ArgumentNamespace(2, const_cast<char **>(input_args));
     REQUIRE(!args.have_correct_number_of_filenames());
+    REQUIRE_THROWS_AS(args.grid_filename(), std::runtime_error);
+    REQUIRE_THROWS_AS(args.output_filename(), std::runtime_error);
   }
   SECTION("Four") {
     const char *input_args[] = {"tdms", "input_file.mat", "grid_file.mat",
                                 "output_file.mat", "fourth_file.mat"};
     auto args = ArgumentNamespace(5, const_cast<char **>(input_args));
     REQUIRE(!args.have_correct_number_of_filenames());
+  }
+}
+
+/**
+ * @brief Test of the CLI options.
+ *
+ * TODO: FD will need to be removed.
+ */
+TEST_CASE("CL options") {
+  vector<string> input_args = {"tdms", "input_file.mat", "output_file.mat"};
+  SECTION("Doesn't have flags") {
+    auto args =
+            ArgumentNamespace(input_args.size(),
+                              const_cast<char **>(vector_to_array(input_args)));
+    REQUIRE(!args.finite_difference());
+    REQUIRE(!args.cubic_interpolation());
+  }
+
+  SECTION("Interpolation flag") {
+    input_args.push_back("--cubic-interpolation");
+    auto args =
+            ArgumentNamespace(input_args.size(),
+                              const_cast<char **>(vector_to_array(input_args)));
+    REQUIRE(!args.finite_difference());
+    REQUIRE(args.cubic_interpolation());
+  }
+  SECTION("Solver method flag") {
+    input_args.push_back("--finite-difference");
+    auto args =
+            ArgumentNamespace(input_args.size(),
+                              const_cast<char **>(vector_to_array(input_args)));
+    REQUIRE(args.finite_difference());
+    REQUIRE(!args.cubic_interpolation());
+  }
+  SECTION("Interpolation quick option") {
+    input_args.push_back("-c");
+    auto args =
+            ArgumentNamespace(input_args.size(),
+                              const_cast<char **>(vector_to_array(input_args)));
+    REQUIRE(!args.finite_difference());
+    REQUIRE(args.cubic_interpolation());
+  }
+
+  SECTION("Solver method quick option") {
+    input_args.push_back("-fd");
+    auto args =
+            ArgumentNamespace(input_args.size(),
+                              const_cast<char **>(vector_to_array(input_args)));
+    REQUIRE(args.finite_difference());
+    REQUIRE(!args.cubic_interpolation());
   }
 }
