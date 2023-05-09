a4bb6cf98552c62a08c938c749d0ee00bd265db9
willGraham01 <1willgraham@gmail.com>
2023-05-09 11:48:04 +0100

    Update paths now we need HDF5 data as well as matlab data

--- tdms/tests/unit/benchmark_scripts/setup_unit_tests.m
+++ tdms/tests/unit/benchmark_scripts/setup_unit_tests.m
@@ -1,7 +1,9 @@
-if ~exist('matlab_data', 'dir')
-    mkdir('matlab_data');
+if ~exist('unit_test_data', 'dir')
+    mkdir('unit_test_data');
 end
 
 create_structure_array;
 create_tdms_object_data;
 create_bad_tdms_object_data;
+
+exit;
