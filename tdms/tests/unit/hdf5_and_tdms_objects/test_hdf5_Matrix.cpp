ec7a004757da92ee7052d91ca086a2f3b818d5de
Sam Cunliffe <samcunliffe@users.noreply.github.com>
2023-05-23 12:27:39 +0100

    `HDF5` file structure reorganisation, and `InterfaceComponent` readability (#312)
    * Handle structs (except with complex data, that's nasty) (#278)
    
    * Trying to decipher structs
    
    * Further towards reading a struct
    
    * Bypass the stack-smash bug.
    
    MATLAB saves character arrays as uint16s, and HDF5 is unable to interpret them as chars. As such, we need to do manual conversion in the test.
    
    * Rename binary and update contents
    
    * `HDF5` file structure reorganisation, and `InterfaceComponent` readability (#280)
    
    * Reorganise hdf5 unit tests
    
    - Move uint16 to char/str functions to the unit test utils file
    - Create hdf5_and_tdms_objects subdirectory of unit/ to hold unit tests on interaction between hdf5 and tdms classes
    - Move the Matrix<double> test from test_hdf5_io into the new subdirectory
    - Data files needed for unit tests are defined in a unit_test_utils namespace to avoid redefinition across multiple files
    
    * Create file to test interface and hdf5 interactions
    
    - Add docstrings to interface.h since these are missing and I've just had to work out what they do
    - Create a matlab script that can reproduce the class_data.mat file which the hdf5 unit tests will try to create tdms objects from
    - Create the barebones test_hdf5_interface file
    
    * HDF5Reader can read from .mat file and produce an InterfaceComponent
    
    * File restructure: accounting for how many tests we are going to have with HDF5
    
    * Prune includes
    
    * Add .mat file for HDF5-TDMS-object unit tests to run.
    
    - Adds scripts to reproduce this data, so in theory a new user can run a short MATLAB script to reproduce this
    - Had a play with trying to get setup-matlab to run these scripts before the unit tests, but alas, no.
    
    * Some tidyup from the comments of PR #312.
    
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>
    Co-authored-by: willGraham01 <1willgraham@gmail.com>

---
+++ tdms/tests/unit/hdf5_and_tdms_objects/test_hdf5_Matrix.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file test_hdf5_Matrix.cpp
+ * @brief Tests of the HDF5 file I/O functionality when reading/writing Matrix
+ * objects.
+ */
+#include "hdf5_io/hdf5_reader.h"
+#include "hdf5_io/hdf5_writer.h"
+
+#include <filesystem>
+
+// external
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+// tdms
+#include "arrays.h"
+#include "unit_test_utils.h"
+
+using std::filesystem::remove_all;
+using tdms_tests::create_tmp_dir;
+
+TEST_CASE("HDF5: Read/Write Matrix") {
+  // test-case wide setup - temporary directory
+  auto tmp = create_tmp_dir();
+
+  SECTION("5-by-6 2D array [double]") {
+    SPDLOG_INFO("5-by-6 2D array");
+    Matrix<double> counting_matrix(5, 6);
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 6; j++) { counting_matrix[i][j] = 6. * i + j; }
+    }
+    Matrix<double> read_back;
+
+    {
+      HDF5Writer f1(tmp.string() + "/five-by-six.h5");
+      f1.write("five-by-six", counting_matrix);
+    }
+    {
+      SPDLOG_DEBUG("About to read...");
+      HDF5Reader f2(tmp.string() + "/five-by-six.h5");
+      f2.read("five-by-six", read_back);
+    }
+
+    for (unsigned int i = 0; i < 5; i++) {
+      for (unsigned int j = 0; j < 6; j++) {
+        SPDLOG_INFO("Checking {} == {}", counting_matrix[i][j],
+                    read_back[i][j]);
+        CHECK(counting_matrix[i][j] == Catch::Approx(read_back[i][j]));
+      }
+    }
+  }
+
+  // teardown - remove temporary directory and all files
+  SPDLOG_DEBUG("Removing temporary directory.");
+  remove_all(tmp);
+}
